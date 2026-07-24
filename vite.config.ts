d383a989079153eaa008b2f5d7b919f2aaab29e2
dodne <soh02052@gmail.com>
2026-07-24 08:06:24 +0000

    Deploy compiled Vite React production build to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,11 @@ export default defineConfig(() => {
   return {
     base: './',
     plugins: [react(), tailwindcss()],
+    build: {
+      rollupOptions: {
+        input: path.resolve(__dirname, 'src/index.html'),
+      },
+    },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
